21f604b547b386d9c5cee163c9f9404cf0798cec
Sejio <seijokopeoiej@gmail.com>
2026-05-04 08:53:42 +0800

    Add files via upload

--- src/MediaScreenClient.java
+++ src/MediaScreenClient.java
@@ -3,6 +3,7 @@ package com.mediascreen;
 import com.mediascreen.block.MediaScreenBlock;
 import com.mediascreen.block.MediaScreenBlockEntity;
 import com.mediascreen.block.ModBlockEntities;
+import com.mediascreen.client.MediaScreenLogger;
 import com.mediascreen.client.MediaScreenRenderer;
 import com.mediascreen.client.MediaScreenScreen;
 import com.mediascreen.client.VideoManager;
@@ -16,12 +17,19 @@ public class MediaScreenClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		MediaScreenLogger.clearLog();
 		BlockEntityRendererRegistry.register(ModBlockEntities.MEDIA_SCREEN, MediaScreenRenderer::new);
 		MediaScreenBlock.openScreenClient = mse -> {
 			MinecraftClient.getInstance().setScreen(new MediaScreenScreen(mse));
 		};
 
+		int[] tickCounter = {0};
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			tickCounter[0]++;
+			if (tickCounter[0] >= 100) {
+				tickCounter[0] = 0;
+				VideoManager.cleanup();
+			}
 			for (VideoPlayer player : VideoManager.getAllPlayers()) {
 				if (player.getTexture() != null) {
 					player.getTexture().tickUpload();
